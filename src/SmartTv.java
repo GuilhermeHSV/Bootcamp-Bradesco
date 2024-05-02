393e5d890cb7217eca5de337387705ebe3ee23e7
Guilherme <guivascon05@gmail.com>
2024-05-02 11:30:20 -0300

    Adicionando os primeiros exercicios

---
+++ src/SmartTv.java
@@ -0,0 +1,25 @@
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 20;
+
+    public void ligar() {
+        ligada = true;
+    }
+
+    public void desligar() {
+        ligada = false;
+    }
+
+    public void aumentarVolume() {
+        volume++;
+    }
+
+    public void diminuirVolume() {
+        volume--;
+    }
+
+    public void aumentarCanal(int numeroCanal) {
+        canal = numeroCanal;
+    }
+}
---
+++ src/Usuario.java
@@ -0,0 +1,21 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+        System.out.println("A TV está ligada? " + smartTv.ligada);
+        System.out.println("O volume da tv está em: " + smartTv.volume);
+        System.out.println("O canal atual é: " + smartTv.canal);
+
+        smartTv.ligar();
+        System.out.println("A TV agora está: " + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("O volume atual é: " + smartTv.volume);
+
+        smartTv.aumentarCanal(23);
+        System.out.println("Canal atual é: " + smartTv.canal);
+
+    }
+}
